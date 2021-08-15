4777dbf475c6434b5abc07bd2ea9ba82b8799fc3
JaeKyungHeo <jaekyung507@naver.com>
2021-08-15 16:29:11 +0900

    파일삭제

--- Chap01_JavaStart/src/ch01_3_Java_process/Hello.java
+++
@@ -1,9 +0,0 @@
-package ch01_3_Java_process;
-
-public class Hello {
-	public static void main(String[] args) {// main + ctrl + space + enter
-		System.out.println("Hello, Java");
-		System.out.println();// 단축키 : syso + ctrl + space
-		// 줄 변경 단축키 : Alt + 위, 아래 방향키
-	}
-}
